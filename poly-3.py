6ceb55f869ac10e1202750ad4cc739e2875d3721
Brian Lam <blam@fb.com>
2019-06-19 15:00:00 -0700

    change line weight. I'm pretty happy with this right now.

--- poly-3.py
+++ poly-3.py
@@ -1,4 +1,5 @@
 from pyx import *
+from sets import Set
 import random
 import sys
 import math
@@ -13,46 +14,49 @@ WIDTH = 600
 HEIGHT = 30
 INIT_M = 0.3
 RATIO = .013
-PRINT_DOTS = 1
+PRINT_DOTS = 0
 PRINT_LINES = True
 
-c.stroke(path.rect(0, 0, WIDTH + 1, HEIGHT + 1))
 
-r = path.rect(1, 1, WIDTH, HEIGHT)
-c.stroke(r,[style.linewidth(0.4)])
 x = 0
 current_x = 0
 points_list = []
 while current_x < WIDTH:
     used_x = x**(1.5)
     num_dots = INIT_M**(used_x*RATIO) * 100
-    y_coords = random.sample(range(HEIGHT), min(HEIGHT, int(math.ceil(num_dots))))
+    y_coords = random.sample(range(1, HEIGHT, 2), min(HEIGHT/2, int(math.ceil(num_dots))))
     for y_coord in y_coords:
-        if PRINT_DOTS:
-            circle = path.circle(used_x, y_coord, 0.5)
-            c.fill(circle)
         points_list.append((used_x, y_coord))
     current_x = used_x
     x += .5
+r = path.rect(1, 1, current_x, HEIGHT - 2)
+c.stroke(r,[style.linewidth(0.4)])
 
+filled_points = Set(filter(lambda point: point[0] < 1**1.5, points_list))
 line_x = 0
 while line_x < x:
     line_used_x = line_x**(1.5)
-    curr_points = filter(lambda point: point[0] == line_x**1.5, points_list)
-    eligible_points = filter(lambda point: point[0] > (line_x)**(1.5) and point[0] < (line_x + 4)**(1.5), points_list)
+    curr_points = filter(lambda point: point[0] == line_x**1.5, filled_points)
+    eligible_points = filter(lambda point: point[0] > (line_x)**(1.5) and point[0] < (line_x + 3)**(1.5), points_list)
+    print len(eligible_points)
     if len(eligible_points) == 0:
         break
     random.shuffle(curr_points)
     random.shuffle(eligible_points)
-    longer_list = curr_points if len(curr_points) > len(eligible_points) else eligible_points
-    shorter_list = curr_points if len(curr_points) < len(eligible_points) else eligible_points
-    for i in range(len(longer_list)):
-        curr_point = longer_list[i]
-        point_to_match = shorter_list[i % len(shorter_list)]
-        c.stroke(
-            path.line(curr_point[0], curr_point[1], point_to_match[0], point_to_match[1]),
-            [style.linecap.round, style.linewidth(0.4)],
-        )
+    for a in curr_points:
+        points_to_connect = random.sample(eligible_points, min(len(eligible_points), 2))
+        filled_points.update(points_to_connect)
+        for b in points_to_connect:
+            c.stroke(
+                path.line(a[0], a[1], b[0], b[1]),
+                [style.linecap.round, style.linewidth(0.75)],
+            )
+
     line_x += .5
+
+if PRINT_DOTS:
+    for point in filled_points:
+        circle = path.circle(point[0], point[1], 0.25)
+        c.fill(circle)
 
 c.writePDFfile("output/poly-3/poly-%d" % (seed));
